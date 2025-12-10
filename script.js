f9d854cc1d3dae953a366b27c7339b353cd237fc
Yeabsra97 <Yeabsra.s.hailu97@gmail.com>
2025-12-10 22:03:00 +0300

    feat: add DOM elements for all buttons in calculator

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// DOM Elements for Numbers
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const sic = document.querySelector("sic");
+
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+
+const zero = document.querySelector(".zero");
+const dot = document.querySelector(".dot");
+
+// DOM Element for operation
+
+const addition = document.querySelector(".addition");
+const subtraction = document.querySelector(".subtraction");
+const division = document.querySelector(".division");
+const multiplication = document.querySelector(".multiplication");
+const equals = document.querySelector(".equal");
+
+//DOM Element for other parts
+const result = document.querySelector(".result");
+const ac = document.querySelector(".AC");
+const doubleZero = document.querySelector(".doubleZero");
+const percent = document.querySelector(".percent");
+const clear  = document.querySelector(".clear");
