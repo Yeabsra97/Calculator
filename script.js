327835d422ff7bd42cfc19794ccda4fdac71bda7
Yeabsra97 <Yeabsra.s.hailu97@gmail.com>
2025-12-11 09:25:53 +0300

    feat: implement basic calculator input handling and evaluation functions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const nine = document.querySelector(".nine");
 
 const four = document.querySelector(".four");
 const five = document.querySelector(".five");
-const sic = document.querySelector("sic");
+const six = document.querySelector(".six");
 
 const one = document.querySelector(".one");
 const two = document.querySelector(".two");
@@ -21,7 +21,7 @@ const addition = document.querySelector(".addition");
 const subtraction = document.querySelector(".subtraction");
 const division = document.querySelector(".division");
 const multiplication = document.querySelector(".multiplication");
-const equals = document.querySelector(".equal");
+const equals = document.querySelector(".equals");
 
 //DOM Element for other parts
 const result = document.querySelector(".result");
@@ -29,3 +29,53 @@ const ac = document.querySelector(".AC");
 const doubleZero = document.querySelector(".doubleZero");
 const percent = document.querySelector(".percent");
 const clear  = document.querySelector(".clear");
+
+
+function append(event){
+    result.textContent += event.target.textContent;
+}
+
+// These event handler append the clicked number to the result
+zero.addEventListener("click", append);
+one.addEventListener("click", append);
+two.addEventListener("click", append);
+three.addEventListener("click", append);
+four.addEventListener("click", append);
+five.addEventListener("click", append);
+six.addEventListener("click", append);
+seven.addEventListener("click", append);
+eight.addEventListener("click", append);
+nine.addEventListener("click", append);
+doubleZero.addEventListener("click", append);
+percent.addEventListener("click", append);
+dot.addEventListener("click", append);
+
+function AC(){
+    result.textContent = "";
+}
+
+
+//Need to use better function than eval
+function Equals(event){
+    try{
+        result.textContent = eval(result.textContent);
+    } catch{
+        result.textContent = "error";
+    }
+    
+}
+
+function Clear(){
+    result.textContent = result.textContent.slice(0, result.textContent.length-1);
+}
+
+addition.addEventListener("click", append);
+subtraction.addEventListener("click", append);
+division.addEventListener("click", append);
+multiplication.addEventListener("click", append);
+clear.addEventListener("click", Clear)
+ac.addEventListener("click", AC);
+equals.addEventListener("click", Equals);
+
+
+
